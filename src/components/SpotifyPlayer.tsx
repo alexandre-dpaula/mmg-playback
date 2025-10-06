1052e56c41247ba8ceaa554ff6ef6342e8345308
[dyad] <contato.m2bstudio@gmail.com>
2025-10-06 17:54:55 -0300

    [dyad] Atualizando a imagem da capa - wrote 1 file(s)

--- src/components/SpotifyPlayer.tsx
+++ src/components/SpotifyPlayer.tsx
@@ -194,7 +194,7 @@ const SpotifyPlayer: React.FC = () => {
         <div className="flex flex-1 flex-col gap-4 sm:gap-6">
           <div className="flex flex-row items-center gap-6">
             <img
-              src="https://i.pinimg.com/736x/ec/9b/b2/ec9bb2fde5e3c2576.jpg"
+              src="https://i.pinimg.com/736x/ec/9b/b2/ec9bb2fde5e3cbba195ee0db0e3d2576.jpg"
               alt="Capa do áudio"
               className="h-32 w-32 sm:h-44 sm:w-44 rounded-2xl object-cover shadow-[0_20px_45px_-20px_rgba(0,0,0,0.8)]"
             />
